11bcd1ffea55f1b33c747137737ea8fa171a09b4
Joe <leethfc11@gmail.com>
2024-05-10 20:09:41 +0900

    Initial Commit

---
+++ movieFinder/movieFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieFinderApp.swift
+//  movieFinder
+//
+//  Created by Joe Lee on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct movieFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
