e9e8d8f643d788bc2221398e738d5eeb6522e0af
Joe <leethfc11@gmail.com>
2024-05-24 00:59:38 +0900

    Long day,,.

---
+++ movieFinder/Categories/CaterogrySearch.swift
@@ -0,0 +1,18 @@
+//
+//  CaterogrySearch.swift
+//  movieFinder
+//
+//  Created by Joe Lee on 5/23/24.
+//
+
+import SwiftUI
+
+struct CaterogrySearch: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CaterogrySearch()
+}
